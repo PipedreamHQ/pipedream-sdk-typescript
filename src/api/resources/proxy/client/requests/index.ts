e1ee20cb259b59d9da5f27652314f21436c224f0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-28 21:28:00 +0000

    SDK regeneration

--- src/api/resources/proxy/client/requests/index.ts
+++
@@ -1,5 +0,0 @@
-export { type ProxyGetRequest } from "./ProxyGetRequest.js";
-export { type ProxyPostRequest } from "./ProxyPostRequest.js";
-export { type ProxyPutRequest } from "./ProxyPutRequest.js";
-export { type ProxyDeleteRequest } from "./ProxyDeleteRequest.js";
-export { type ProxyPatchRequest } from "./ProxyPatchRequest.js";
