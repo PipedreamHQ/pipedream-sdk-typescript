b51f73afe4e6813e592579b9f1ed400f01870c50
Jay Vercellone <jay@pipedream.com>
2025-10-16 16:17:03 -0300

    Fix build issues

--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -1,6 +1,6 @@
 export { BasicAuth } from "./BasicAuth.js";
 export { BearerToken } from "./BearerToken.js";
-export { ConnectTokenProvider, TokenCallback } from "./ConnectTokenProvider.js";
+export { ConnectTokenProvider, type TokenCallback } from "./ConnectTokenProvider.js";
 export { OAuthTokenProvider } from "./OAuthTokenProvider.js";
 export { StaticTokenProvider } from "./StaticTokenProvider.js";
-export { TokenProvider } from "./TokenProvider.js";
+export type { TokenProvider } from "./TokenProvider.js";
