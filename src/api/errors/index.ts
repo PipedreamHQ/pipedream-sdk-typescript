40572b4095df8089452effe54bcb1281a476bf70
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-17 17:11:31 +0000

    [fern-generated] Update SDK
    Generated by Fern
    CLI Version: unknown
    Generators:
      - fernapi/fern-typescript-node-sdk: 3.69.1

--- src/api/errors/index.ts
+++ src/api/errors/index.ts
@@ -1,3 +1,5 @@
 export * from "./BadRequestError.js";
 export * from "./NotFoundError.js";
+export * from "./NotImplementedError.js";
+export * from "./ServiceUnavailableError.js";
 export * from "./TooManyRequestsError.js";
